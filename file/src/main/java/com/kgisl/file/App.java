cfd00f2b467161d40f2045e6a48bc93de728e239
Praveen. p <85276447+Praveenp-maker@users.noreply.github.com>
2023-01-27 10:19:43 +0530

    new

--- file/src/main/java/com/kgisl/file/App.java
+++ file/src/main/java/com/kgisl/file/App.java
@@ -1,5 +1,14 @@
 package com.kgisl.file;
 
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 /**
  * Hello world!
  *
@@ -7,7 +16,42 @@ package com.kgisl.file;
 public class App 
 {
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    {  
+         Spacereduce op=new Spacereduce();
+       String line="";
+       String splitBy=",";
+       List<Model> modelList = new ArrayList<Model>();
+       try{
+        BufferedReader br=new BufferedReader(new FileReader("D:\\praveen\\java\\file\\src\\main\\resources\\worldcities.csv"));
+        while((line=br.readLine())!=null){
+            String[]model=line.split(splitBy);
+            // System.out.println(model[0]+""+model[1]);
+            modelList.add(new Model(model[0], model[1], model[2], model[3], model[4], model[5], model[6], model[7], model[8], model[9], model[10]));
+        }
+       }catch(IOException e){
+        e.printStackTrace();
+       }
+       //System.out.println(modelList.get(4)+">>>>"+modelList.get(8));
+       //op.countryWishCityCount(modelList);
+    //modelList.stream().map(p->p.getCountry()+" "+p.getCity()).forEach(System.out::println);
+// System.out.println(modelList);
+      
+        Map mp=new HashMap<String,Integer>();
+        for (Model m:modelList) {
+            String key=m.getCountry();
+           int count=0;
+            // List li=new ArrayList<String>();
+            for (Model model : modelList) {
+                while(key.equals(model.getCountry())){
+                    // li.add(model.getCity());
+                    count=count+1;
+                }
+            }
+            mp.put(key, count);
+        }
+      //  System.out.println(mp);
     }
-}
+       }
+    
+  
+   
